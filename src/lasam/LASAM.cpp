b67f0ac3d904d618f84ad911b902d95cc75a22b2
Gabe <gabrieljameskeller@gmail.com>
2021-05-11 14:11:12 -0500

    Initial

---
+++ src/lasam/LASAM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gabriel on 11/05/2021.
+//
+
+#include "LASAM.h"
---
+++ src/lasam/LASAM.h
@@ -0,0 +1,14 @@
+//
+// Created by Gabriel on 11/5/2021.
+//
+
+#ifndef LASAM_LASAM_H
+#define LASAM_LASAM_H
+
+
+class LASAM {
+
+};
+
+
+#endif //LASAM_LASAM_H
